0abbf8ad59b977f881fbbdceca1d7b5cb1d564e6
VadosLight <it-vadim@inbox.ru>
2019-09-24 12:01:20 +0300

    1-st commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Lab1_forms_golceva
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.txtG = new System.Windows.Forms.TextBox();
+            this.txtV = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(591, 363);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.txtM);
+            this.tabPage1.Controls.Add(this.txtG);
+            this.tabPage1.Controls.Add(this.txtV);
+            this.tabPage1.Controls.Add(this.txtC);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.txtN);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(583, 337);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Аналитическая модель";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 133);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Расчёт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtM
+            // 
+            this.txtM.Location = new System.Drawing.Point(243, 106);
+            this.txtM.Margin = new System.Windows.Forms.Padding(2);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(135, 20);
+            this.txtM.TabIndex = 9;
+            this.txtM.Text = "10";
+            this.txtM.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtN_KeyPress);
+            // 
+            // txtG
+            // 
+            this.txtG.Location = new System.Drawing.Point(243, 83);
+            this.txtG.Margin = new System.Windows.Forms.Padding(2);
+            this.txtG.Name = "txtG";
+            this.txtG.Size = new System.Drawing.Size(135, 20);
+            this.txtG.TabIndex = 8;
+            this.txtG.Text = "15";
+            this.txtG.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtN_KeyPress);
+            // 
+            // txtV
+            // 
+            this.txtV.Location = new System.Drawing.Point(243, 57);
+            this.txtV.Margin = new System.Windows.Forms.Padding(2);
+            this.txtV.Name = "txtV";
+            this.txtV.Size = new System.Drawing.Size(135, 20);
+            this.txtV.TabIndex = 7;
+            this.txtV.Text = "150";
+            this.txtV.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtN_KeyPress);
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(243, 32);
+            this.txtC.Margin = new System.Windows.Forms.Padding(2);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(135, 20);
+            this.txtC.TabIndex = 6;
+            this.txtC.Text = "10";
+            this.txtC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtN_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(6, 106);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(233, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "M коээфициент продолжительности реакции";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(243, 9);
+            this.txtN.Margin = new System.Windows.Forms.Padding(2);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(135, 20);
+            this.txtN.TabIndex = 4;
+            this.txtN.Text = "2";
+            this.txtN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtN_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(6, 83);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(233, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "G объемный расход, м^3/Ч";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(6, 57);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "V объем аппарата, м^3";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(6, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "C(вх) концентрация входная, Кмоль/м^3";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(6, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N число ячеек в реакторе";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.chart1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(583, 337);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "График";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineColor = System.Drawing.Color.Black;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(2, 2);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(579, 333);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage3.Size = new System.Drawing.Size(583, 337);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Численная модель";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Компьютерное моделирование в химии и химической технологии";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.TextBox txtG;
+        private System.Windows.Forms.TextBox txtV;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab1_forms_golceva
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double Math_Model(int N, double M, double C, double V, double G, double tau, double time)
+        {
+            double C_vyh = 0;
+            switch (N)
+            {
+                case 1:
+                    C_vyh = C - (1 + (time / tau))
+                        * Math.Exp((-time / tau)) * C;
+                    break;
+
+                case 2:
+                    C_vyh = C - (1 + (time / tau)
+                        + 0.5 * Math.Pow((time / tau), 2))
+                        * Math.Exp((-time / tau)) * C;
+                    break;
+
+                case 3:
+                    C_vyh = C - (1 + (time / tau)
+                        + 0.5 * Math.Pow((time / tau), 2)
+                        + (1 / 6) * Math.Pow((time / tau), 3))
+                        * Math.Exp((-time / tau)) * C;
+                    break;
+            }
+
+            return C_vyh;
+        }
+
+        private double Math_Model_2(int N, double M, double C, double V, double G, double tau, double time)
+        {
+            double C_vyh = 0;
+            switch (N)
+            {
+                case 1:
+                    C_vyh = (1 + (time / tau))
+                        * Math.Exp((-time / tau)) * C;
+                    break;
+
+                case 2:
+                    C_vyh = (1 + (time / tau)
+                        + 0.5 * Math.Pow((time / tau), 2))
+                        * Math.Exp((-time / tau)) * C;
+                    break;
+
+                case 3:
+                    C_vyh = (1 + (time / tau)
+                        + 0.5 * Math.Pow((time / tau), 2)
+                        + (1 / 6) * Math.Pow((time / tau), 3))
+                        * Math.Exp((-time / tau)) * C;
+                    break;
+            }
+
+            return C_vyh;
+        }
+        private void Calculate(int N, double M, double C, double V, double G, double tau, double time)
+        {
+            double step = 1;
+            //===============
+            chart1.Series.Clear();
+            chart1.Dock = DockStyle.Fill;
+
+            Series mySeriesOfPoint = new Series("Модель реактора");
+            mySeriesOfPoint.ChartType = SeriesChartType.Line;
+            //====================
+            for (double x = 0; x <= time; x += step)
+            {
+                double y = Math_Model(N, M, C, V, G, tau, x);
+                mySeriesOfPoint.Points.AddXY(x, y);
+            }
+
+            double last_С =  Math_Model_2(N, M, C, V, G, tau, time);
+            mySeriesOfPoint.Points.AddXY(time, last_С);
+            C = last_С;
+
+            for (double x = 0; x <= time; x += step)
+            {
+                double y = Math_Model_2(N, M, C, V, G, tau, x);
+                mySeriesOfPoint.Points.AddXY(x+time, y);
+            }
+
+            //====================
+            {
+                Axis ax = new Axis();
+                ax.Title = "Время";
+                chart1.ChartAreas[0].AxisX = ax;
+                Axis ay = new Axis();
+                ay.Title = "Выходная концентрация";
+                chart1.ChartAreas[0].AxisY = ay;
+            }
+            chart1.Series.Add(mySeriesOfPoint);
+        }
+
+        private void TxtN_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8 && number != 44) //цифры, клавиша BackSpace и запятая а ASCII
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int N = Int16.Parse(txtN.Text);
+                double C = Double.Parse(txtC.Text);
+                double V = Double.Parse(txtV.Text);
+                double G = Double.Parse(txtG.Text);
+                double M = Double.Parse(txtM.Text);
+                double tau = 0;
+                double time = 0;
+
+
+                if (G != 0 && V != 0 && M > 0)
+                {
+                    tau = V / G;
+                    time = tau * M;
+                }
+                else
+                {
+                    MessageBox.Show("Деление на ноль");
+                    return;
+                }
+
+                if (1 <= N && N <= 3)
+                {
+                    Calculate(N, M, C, V, G, tau, time);
+                }
+                else
+                {
+                    MessageBox.Show("N должно быть от 1 до 3");
+                    return;
+                }
+
+
+            }
+            catch
+            {
+                MessageBox.Show("При вводе данных возникла ошибка");
+            }
+        }
+    }
+}
